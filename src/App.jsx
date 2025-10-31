b255ea6521e2d95579c996cc71adb91d396b6ac7
Sira Sasisakulporn <sira2violin@gmail.com>
2025-10-31 16:22:34 +0700

    create pages, connect to NASA API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import SideBar from "./components/SideBar"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+import { useState, useEffect } from "react"
+
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  
+  function handleToggleModal() {
+    setShowModal(!showModal);
+  }
+
+  useEffect(() => {
+    async function fectchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      console.log(NASA_KEY);
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log('DATA\n', data);
+      } catch(err) {
+        console.log(err.message);
+      }
+    }
+    fectchAPIData();
+  }, [])
 
   return (
     <>
-      {8*4}
+      <Main/>
+      {showModal && (
+        <SideBar handleToggleModal={handleToggleModal}/>
+      )}
+      <Footer handleToggleModal={handleToggleModal} />
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <div className="imageContainer">
+            <img src="space.jpg" alt="space-demo-picture" className="bgImage"/>
+        </div>
+    );
+}
