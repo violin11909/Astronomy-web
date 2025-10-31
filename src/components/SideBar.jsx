7d96092dd0cb4ac0e0ba1d7cd93f1607a4b57d17
Sira Sasisakulporn <sira2violin@gmail.com>
2025-10-31 16:44:16 +0700

    use data from api, still have to refactor the pages

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,14 @@
 export default function SideBar(props) {
-    const {handleToggleModal} = props;
+    const {handleToggleModal, data} = props;
     
     return (
         <div className="sidebar">
             <div  onClick={handleToggleModal} className="bgOverlay"></div>
             <div className="sidebarContents">
-                <h2>The Space</h2>
-                <div>
-                    <p>Description</p>
-                    <p>adfglkfsjdgl kjlkgfjsdkl gjkfdsj gksfjd; kgjks df;g</p>
+                <h2>{data?.title}</h2>
+                <div className="descriptionContainer">
+                    <p className="descriptionTitle">{data?.date}</p>
+                    <p>{data?.explanation}</p>
                 </div>
                 <button onClick={handleToggleModal}>
                     <i className="fa-solid fa-arrow-right"></i>
